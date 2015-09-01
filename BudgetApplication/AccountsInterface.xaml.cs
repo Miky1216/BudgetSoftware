0aba6fb3f0bbe181fb494f0b45ea5de07cd8c491
unknown <Mterrill_Li@dev5.bsecode.inc>
2015-09-01 13:57:20 -0500

    Initial commit and working navigation among pages.

---
+++ BudgetApplication/AccountsInterface.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BudgetApplication
+{
+    /// <summary>
+    /// Interaction logic for AccountsInterface.xaml
+    /// </summary>
+    public partial class AccountsInterface : System.Windows.Controls.Page
+    {
+        public AccountsInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccountAccount_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBudgetAccount_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReportsAccount_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BudgetApplication/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BudgetApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Controls.Page
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnAccount_Click(object sender, RoutedEventArgs e)
+        {
+            AccountsInterface accountInterface = new AccountsInterface();
+            
+            
+        }
+
+        private void btnBudget_Click(object sender, RoutedEventArgs e)
+        {
+            BudgetInterface budgetInterface = new BudgetInterface();
+            
+        }
+
+        private void btnReports_Click(object sender, RoutedEventArgs e)
+        {
+            ReportsInterface reportInterface = new ReportsInterface();
+            
+        }
+    }
+}
