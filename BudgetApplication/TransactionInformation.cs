2a3d0df13185b3b3c8fe66f38ee3c160b1f01357
unknown <MTerrill_Li@dev5.bsecode.inc>
2015-09-03 11:01:31 -0500

    Writing to XML file

---
+++ BudgetApplication/TransactionInformation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApplication
+{
+    public class TransactionInformation
+    {
+        private string date;
+        private string item;
+        private string amount;
+        private string depositWithdrawal;
+        //private string transactions;
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+            }
+        }
+        public string Item
+        {
+            get
+            {
+                return item;
+            }
+            set
+            {
+                item = value;
+            }
+        }
+        public string Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
+        public string DepositWithdrawal
+        {
+            get
+            {
+                return depositWithdrawal;
+            }
+            set
+            {
+                depositWithdrawal = value;
+            }
+        }
+    }
+}
